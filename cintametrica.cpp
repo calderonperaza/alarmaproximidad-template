4cc7a3ddb8fdc7a8a76f064537da43ca2ddd9870
vc20009ESD1352021 <90218787+vc20009ESD1352021@users.noreply.github.com>
2023-03-25 16:51:21 -0600

    tarea

--- cintametrica.cpp
+++ cintametrica.cpp
@@ -4,8 +4,41 @@
 // ****** CLASE ULTRASONIDO  **********
 //Constructor parametrizado
 
-Ultrasonido::Ultrasonido(){};
+Ultrasonido::Ultrasonido( byte _pinTrigger, byte _pinEcho){
+    pinTrigger=_pinTrigger;
+    pinEcho=_pinEcho;
+    pinMode(_pinTrigger, OUTPUT);
+    pinMode(_pinEcho, INPUT);
+}
 
+Ultrasonido::Ultrasonido(){};
+int Ultrasonido::medirCM(){
+    digitalWrite(pinTrigger, LOW);
+    delayMicroseconds(4);
+    digitalWrite(pinTrigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(pinTrigger, LOW);
+    distancia=pulseIn(pinEcho,HIGH)/56.5812;
+    distancia=constrain(distancia,0,350);   
+};
+void Display7::configurar(byte _pines[]){
+     for(byte x=0; x<7; x++){
+        pines[x]=_pines[x];
+        pinMode(pines[x],OUTPUT);
+
+     }
+}
+void Display7::mostrar(byte _num){
+if(_num>9){
+    _num=9;
+}
+byte valor,pin;
+for(byte j=0; j<=7; j++){
+    valor=matrix[_num][j];
+    pin=pines[j];
+    digitalWrite(pin,valor);
+}
+}
 //hace la medición de distancia, devuelve los cmtros de distancia hasta 350 cm maximo
 
 
