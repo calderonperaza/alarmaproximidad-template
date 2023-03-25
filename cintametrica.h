79513467da4981ce8853602f5c59f917767dc678
DiegoMendoz <daygon2001@gmail.com>
2023-03-24 22:27:10 -0600

    validacion de datos

--- cintametrica.h
+++ cintametrica.h
@@ -3,21 +3,38 @@
 #include "arduino.h"  
 
 class Ultrasonido{
- 
+ private:
+ long distancia;
+ public:
+     byte pinEcho;
+ byte pinTrigger;
 //constructor parametrizado, recibe como parametros Trigger y Echo como byte
-
-
-
-
-
+//trigger y Echo como byte
+Ultrasonido (byte _pinTrigger, byte _pinEcho);
+Ultrasonido();
 //Mide la distancia, el sensor es preciso hasta 3.5 metros
 // por tanto el valor maximo es 350 cmtros 
-
+int medirCM();
 };
 
 class Display7{
+private:
+byte matrix[10][7]={
+    {1,1,1,1,1,1,0},
+    {0,1,1,0,0,0,0},
+    {1,1,0,1,1,0,1},
+    {1,1,1,1,0,0,1},
+    {0,1,1,0,0,1,1},
+    {1,0,1,1,0,1,1},
+    {1,1,1,0,0,0,0},
+    {1,1,1,1,1,1,1},
+    {1,1,1,0,0,1,1}
 };
-
-
-
+public:
+byte pines[7];
+//se configura utilizando un metodo
+void configurar(byte[]);
+//se dibuja el numero en el display
+void mostrar(byte num);
+};
 #endif
