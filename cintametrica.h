6e7daaae866f4bfd3f5e79ba1b075a75db132d45
Kevin Cruz <54543628+kevin64diaz@users.noreply.github.com>
2023-03-25 22:46:56 -0600

    Update cintametrica.h

--- cintametrica.h
+++ cintametrica.h
@@ -6,16 +6,37 @@ class Ultrasonido{
  
 //constructor parametrizado, recibe como parametros Trigger y Echo como byte
 
-
-
-
+Ultrasonido(byte _pinTrigger,byte _pinEcho);
+Ultrasonido();
 
 //Mide la distancia, el sensor es preciso hasta 3.5 metros
 // por tanto el valor maximo es 350 cmtros 
 
+ int medirCM();
 };
 
 class Display7{
+ private:
+  byte matrix[10][7] = {
+  //A, B, C, D, E, F, G 
+  { 1, 1, 1, 1, 1, 1, 0 },  // 0
+  { 0, 1, 1, 0, 0, 0, 0 },  // 1
+  { 1, 1, 0, 1, 1, 0, 1 },  // 2
+  { 1, 1, 1, 1, 0, 0, 1 },  // 3
+  { 0, 1, 1, 0, 0, 1, 1 },  // 4
+  { 1, 0, 1, 1, 0, 1, 1 },  // 5
+  { 1, 0, 1, 1, 1, 1, 1 },  // 6
+  { 1, 1, 1, 0, 0, 0, 0 },  // 7
+  { 1, 1, 1, 1, 1, 1, 1 },  // 8
+  { 1, 1, 1, 0, 0, 1, 1 },  // 9
+  }
+};
+ public:
+  byte pines[7];
+ //Se configura utilizando un metodo 
+ void configurar(byte[]);
+ //dibuja el numero en e display
+ void mostrar(byte num);
 };
 
 
