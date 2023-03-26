ac65b2d3eb99ad0da796bd0ad338b105dd6fe355
= <86933089+CarlosPachecoDev@users.noreply.github.com>
2023-03-25 21:26:23 -0600

    Añadiendo código

--- cintametrica.h
+++ cintametrica.h
@@ -3,8 +3,15 @@
 #include "arduino.h"  
 
 class Ultrasonido{
- 
+private:
+    long distancia;
+public:
+    byte pinEcho;
+    byte pinTrigger;
+
 //constructor parametrizado, recibe como parametros Trigger y Echo como byte
+Ultrasonido (byte, byte);
+Ultrasonido();
 
 
 
@@ -12,10 +19,30 @@ class Ultrasonido{
 
 //Mide la distancia, el sensor es preciso hasta 3.5 metros
 // por tanto el valor maximo es 350 cmtros 
+int medirCM();
+
 
 };
 
 class Display7{
+private:
+    byte numbers[10][7]={ 
+	    {1,1,1,1,1,1,0},
+	    {0,1,1,0,0,0,0},
+	    {1,1,0,1,1,0,1},
+	    {1,1,1,1,0,0,1},
+	    {0,1,1,0,0,1,1},
+	    {1,0,1,1,0,1,1},
+	    {1,0,1,1,1,1,1},
+	    {1,1,1,0,0,0,0},
+	    {1,1,1,1,1,1,1},
+	    {1,1,1,0,0,1,1}
+    };
+public:
+    byte pins[7];
+
+void configurar(byte[]);
+void mostrar(byte);
 };
 
 
