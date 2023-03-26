cdd385fee2dca6fd2b0f63c7bca258f03e2ca81e
Alejandro Rivas <alejandrorrh97@gmail.com>
2023-03-26 13:13:38 -0600

    implementacion del codigo para la libreria de arduino

--- cintametrica.cpp
+++ cintametrica.cpp
@@ -1,14 +1,36 @@
 #include "arduino.h"
 #include "cintametrica.h"
 
-// ****** CLASE ULTRASONIDO  **********
-//Constructor parametrizado
-
+Ultrasonido::Ultrasonido(byte _triggerPin, byte _echoPin){
+    triggerPin = triggerPin;
+    echoPin = echoPin;
+    pinMode(_triggerPin, OUTPUT);
+    pinMode(_echoPin, INPUT);
+}
 Ultrasonido::Ultrasonido(){};
 
-//hace la medición de distancia, devuelve los cmtros de distancia hasta 350 cm maximo
+int Ultrasonido::medirDistancia(){
+    digitalWrite(triggerPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(triggerPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(triggerPin, LOW);
+    distancia = pulseIn(echoPin, HIGH) / 58;
+    return distancia;
+}
+
 
 
 // **** CLASE DISPLAY7  ******
-// esta clase no emplea un constructor para configurarse
-// utiliza un metodo
+void Display7::setUp(byte _pines[7]){
+    for(int i = 0; i < 7; i++){
+        pines[i] = _pines[i];
+        pinMode(pines[i], OUTPUT);
+    }
+}
+
+void Display7::display(byte numero){
+    for(int i = 0; i < 7; i++){
+        digitalWrite(pines[i], matrix[numero][i]);
+    }
+}
--- cintametrica.h
+++ cintametrica.h
@@ -1,23 +1,43 @@
 #ifndef cintametrica_h
 #define cintametrica_h
-#include "arduino.h"  
+#include "arduino.h"
 
-class Ultrasonido{
- 
-//constructor parametrizado, recibe como parametros Trigger y Echo como byte
+class Ultrasonido
+{
+    private:
+        int distancia;
 
+    public:
+        byte triggerPin;
+        byte echoPin;
 
+    Ultrasonido(byte _triggerPin, byte _echoPin);
+    Ultrasonido();
 
-
-
-//Mide la distancia, el sensor es preciso hasta 3.5 metros
-// por tanto el valor maximo es 350 cmtros 
-
+    int medirDistancia();
 };
 
-class Display7{
+class Display7
+{
+    private:
+        byte matrix[10][7] = {
+            {1,1,1,1,1,1,0}, //0
+            {0,1,1,0,0,0,0}, //1
+            {1,1,0,1,1,0,1}, //2
+            {1,1,1,1,0,0,1}, //3
+            {0,1,1,0,0,1,1}, //4
+            {1,0,1,1,0,1,1}, //5
+            {1,0,1,1,1,1,1}, //6
+            {1,1,1,0,0,0,0}, //7
+            {1,1,1,1,1,1,1}, //8
+            {1,1,1,1,0,1,1}  //9
+        };
+
+    public:
+        byte pines[7];
+
+    void setUp(byte [7]);
+    void display(byte numero);
 };
-
-
 
 #endif
