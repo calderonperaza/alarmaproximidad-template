92cadf70e78a37c242985abf8fd4ef2d5091561c
InmerArana <79709650+InmerArana@users.noreply.github.com>
2023-03-26 22:23:35 -0600

    Update cintametrica.h

--- cintametrica.h
+++ cintametrica.h
@@ -1,23 +1,37 @@
 #ifndef cintametrica_h
 #define cintametrica_h
-#include "arduino.h"  
+#include "arduino.h"
 
 class Ultrasonido{
- 
-//constructor parametrizado, recibe como parametros Trigger y Echo como byte
+   private:
+	long distancia;
+  public:
+	byte pinEcho;
+	byte pinTrigger;
+Ultrasonido(byte _pinTrigger,byte _pinEcho);
+Ultrasonido();
 
-
-
-
-
-//Mide la distancia, el sensor es preciso hasta 3.5 metros
-// por tanto el valor maximo es 350 cmtros 
-
+int medirCM();
 };
 
 class Display7{
+    private:
+        byte matrix[10][7]={
+ 	{1,1,1,1,1,1,0},
+  	{0,1,1,0,0,0,0},
+ 	 {1,1,0,1,1,0,1},
+ 	 {1,1,1,1,0,0,1},
+	  {0,1,1,0,0,1,1},
+	  {1,0,1,1,0,1,1},
+	  {1,0,1,1,1,1,1},
+	  {1,1,1,0,0,0,0},
+ 	 {1,1,1,1,1,1,1},
+ 	 {1,1,1,0,0,1,1} 
+                                         };
+    public:
+         byte pines[7];
+void configurar(byte[]);
+void mostrar(byte num);
 };
-
-
 
 #endif
